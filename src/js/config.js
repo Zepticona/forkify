1d4dd58ed75d044b9d8e58ab5a08df2f069dc549
zepticona <isakil416@gmail.com>
2021-03-13 08:27:51 +0600

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '4923369d-e620-410e-81f3-a485064056fe';
+export const MODAL_CLOSE_SEC = 2.5;
